64d35b449d84984923d472609008fe143b142ab0
fogsong233 <fogsong233@gmail.com>
2025-11-07 12:06:41 +0800

    Fix config str;

--- src/hook/linux/config.h
+++ src/hook/linux/config.h
@@ -1,10 +1,10 @@
 #pragma once
 
 namespace catter::config {
-constexpr const inline static char* KEY_CMD_LOG_FILE = "__catter_file_to_append_v1";
-constexpr const inline static char* KEY_PRELOAD = "LD_PRELOAD";
-constexpr const inline static char* KEY_CATTER_PRELOAD_PATH = "key_catter_preload_path_v1";
-constexpr const inline static char* ERROR_PREFFIX = "__catter_v1_error:";
+constexpr const inline static char KEY_CMD_LOG_FILE[] = "__catter_file_to_append_v1";
+constexpr const inline static char KEY_PRELOAD[] = "LD_PRELOAD";
+constexpr const inline static char KEY_CATTER_PRELOAD_PATH[] = "key_catter_preload_path_v1";
+constexpr const inline static char ERROR_PREFFIX[] = "__catter_v1_error:";
 constexpr const inline static char OS_PATH_SEPARATOR = ':';
 constexpr const inline static char OS_DIR_SEPARATOR = '/';
 }  // namespace catter::config
