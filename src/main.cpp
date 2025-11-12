c2f356b215f349619805432c3eef91b349dd8548
fogsong233 <fogsong233@gmail.com>
2025-11-12 21:07:46 +0800

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char* argv[]) {
         }
     } else {
         std::println("Failed to collect captured output: {}", captured_output.error());
+        return 1;
     }
     return 0;
 }
